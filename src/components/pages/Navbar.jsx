0c8bf6e0dc7f4af143fdd65d61e222346eed08b8
DrInnovatio <turing1723@gmail.com>
2020-12-07 21:01:44 +1000

    added const showButton.

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -12,6 +12,16 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false )
+
+  const showButton = () => {
+    if (window.innerWidths <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton)
   
   return (
     <div>
