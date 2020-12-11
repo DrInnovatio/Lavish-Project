ed947f1ca19c9e1ff98b57612c9098e90d53fd3d
DrInnovatio <turing1723@gmail.com>
2020-12-11 23:17:39 +1000

    added HereSection files(jsx and css).

--- src/App.js
+++ src/App.js
@@ -15,3 +15,6 @@ function App() {
 }
 
 export default App;
+
+
+//  58:00
---
+++ src/components/pages/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './Button';
+
+function HeroSection() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HeroSection
